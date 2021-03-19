fd0a75f0a41cb4e628020d833a6f1cb12adfd804
Mohamed hedi Chenchen <chenchenmohamedhedi@gmail.com>
2021-03-19 08:11:14 +0100

    first commit

---
+++ src/pages/accueil/Accueil.js
@@ -0,0 +1,17 @@
+import React ,{useEffect , useState} from 'react';
+
+
+function Accueil (props) {
+
+    const [reload, setReload] = useState(true);
+  
+  
+
+ return(
+  <div className="content " >
+      <h1> Accueil </h1>
+  </div>
+ );
+}
+
+export default Accueil;
